f0d32b18f00b2032d89272c3ec35f7e01b34950e
Lucas Allen <lucas.g.allen@gmail.com>
2015-11-06 17:21:27 -0500

    Add overlay and its trigger for contact information.

--- lucasgallen/lucasgallen/static/javascript/global.js
+++ lucasgallen/lucasgallen/static/javascript/global.js
@@ -1,6 +1,14 @@
 (function($) {
     var $siteMenu = $('.site-menu');
 
+    $('#contact-trigger').on('click', function() {
+        $('#overlay').toggleClass('active');
+    });
+
+    $('#overlay-close').on('click', function() {
+        $('#overlay').toggleClass('active');
+    });
+
     $('#small-menu-toggle').on('click', function() {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active');
