b517749c989f909dbff3bced7a52e24809ca1690
Lucas Allen <lucas.g.allen@gmail.com>
2015-11-08 11:17:00 -0500

    Update settings for storing media files in S3.

--- lucasgallen/settings/production.py
+++ lucasgallen/settings/production.py
@@ -3,3 +3,11 @@ import dj_database_url
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_S3_SECURE_URLS = False       # use http instead of https
+AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related query parameters for requests
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
+MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
