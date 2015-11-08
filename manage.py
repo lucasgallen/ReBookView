69186ecfa6e7deda452787371c5ccd51cc48a69c
Lucas Allen <lucas.g.allen@gmail.com>
2015-11-07 23:35:18 -0500

    Check if running on heroku to use production settings.

--- manage.py
+++ manage.py
@@ -4,7 +4,10 @@ import sys
 from pprint import pprint as p
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lucasgallen.settings.local")
+    if 'ON_HEROKU' in os.environ:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lucasgallen.settings.production")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lucasgallen.settings.local")
 
     from django.core.management import execute_from_command_line
 
